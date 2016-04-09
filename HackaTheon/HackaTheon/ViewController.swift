0c835cbf611fe1e1a1d313ff7176ef282f322f8c
Michal Smialko <michal.moriquendi@gmail.com>
2016-04-09 13:15:20 +0200

    Create new empty Xcode project.

---
+++ HackaTheon/HackaTheon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackaTheon
+//
+//  Created by Michal Smialko on 09/04/16.
+//  Copyright © 2016 Michal Smialko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
